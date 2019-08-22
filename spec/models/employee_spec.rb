b3d67c7c369dd04b0616d49fee340238f7df7343
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2019-08-22 16:56:56 -0400

    added scope spec

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -41,5 +41,27 @@ RSpec.describe Employee, type: :model do
     end
   end
 
+  context "scopes" do
+    describe ".zero_balance" do
+      before do 
+        Employee.create!(
+          { first_name: "Jane", last_name: "Doe", rewards_balance: 0 },
+          { first_name: "John", last_name: "Don", rewards_balance: 0 },
+          { first_name: "Adam", last_name: "Michael", rewards_balance: 800 },
+        )
+      end
+
+      it "returns a list of active records" do 
+        result = Employee.zero_balance
+
+        expect(result.count).to eq 2
+        expect(result.first.first_name).to eq "Jane"
+        expect(result.las.first_name).to eq "John"
+        expect(result.any? { |employee| employee.first_name == "Adam" }).to be false
+      end
+    end
+  end
+
+  
 
 end
