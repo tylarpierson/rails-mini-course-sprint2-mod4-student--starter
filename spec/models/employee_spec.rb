c2020583078f220dfb2dfcbe0ab8627bb22aad37
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2019-08-22 16:43:26 -0400

    redid validation specs

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -1,20 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Employee, type: :model do
-  describe "Validations" do
-    context "first and last name" do
-      it { expect(Employee.new(first_name:"Tylar", last_name:"Pierson", rewards_balance: 600)).to be true }
-      it { expect(errors = employee.errors.full_messages).to be_empty }
+  context "Validations" do
+    it "is valid" do 
+      employee = Employee.new(first_name: "Tylar", last_name: "Pierson", rewards_balance: 600)
+      result = employee.valid?
+      errors = employee.errors.full_messages
+
+      expect(result).to be true
+      expect(errors).to be false
     end
-    context "first name nil" do
-      it { expect(Employee.new(first_name:nil, last_name:"Pierson", rewards_balance: 600)).to be false }
-      it { expect(errors = employee.errors.full_messages).to include("First name can't be blank.") }
+
+    it "is invalid without first name" do 
+      employee = Employee.new(first_name: nil, last_name: "Pierson", rewards_balance: 600)
+      result = employee.valid?
+      errors = employee.errors.full_messages
+
+      expect(result).to be false
+      expect(errors).to include("First name can't be blank.")
+
     end
-    context "last name nil" do 
-      it { expect(Employee.new(first_name:"Tylar", last_name:nil, rewards_balance: 600)).to be false }
-      it { expect(errors = employee.errors.full_messages).to include("Last name can't be blank.")}
+
+    it "is invalid without last name" do 
+      employee = Employee.new(first_name: "Tylar", last_name: nil, rewards_balance: 600)
+      result = employee.valid?
+      errors = employee.errors.full_messages
+
+      expect(result).to be true
+      expect(errors).to include("Last name can't be blank.")
     end
   end
 
   
+
+
 end
