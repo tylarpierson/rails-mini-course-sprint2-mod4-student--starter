2386bcf3d21109d8b7e1ffa5ba147a6ae55accb0
Tylar Pierson <tylarpiersonbusiness@gmail.com>
2019-08-22 16:36:37 -0400

    wrote specs for validations

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Employee, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Validations" do
+    context "first and last name" do
+      it { expect(Employee.new(first_name:"Tylar", last_name:"Pierson", rewards_balance: 600)).to be true }
+      it { expect(errors = employee.errors.full_messages).to be_empty }
+    end
+    context "first name nil" do
+      it { expect(Employee.new(first_name:nil, last_name:"Pierson", rewards_balance: 600)).to be false }
+      it { expect(errors = employee.errors.full_messages).to include("First name can't be blank.") }
+    end
+    context "last name nil" do 
+      it { expect(Employee.new(first_name:"Tylar", last_name:nil, rewards_balance: 600)).to be false }
+      it { expect(errors = employee.errors.full_messages).to include("Last name can't be blank.")}
+    end
+  end
+
+  
 end
